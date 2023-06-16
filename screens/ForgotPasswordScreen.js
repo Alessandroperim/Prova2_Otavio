3b65509d4c8066ef79dd57d0a450fc9778f3c0bd
Alessandroperim <78419618+Alessandroperim@users.noreply.github.com>
2023-06-16 12:23:18 -0300

    Add files via upload

---
+++ screens/ForgotPasswordScreen.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const ForgotPasswordScreen = ({ navigation }) => {
+  const handleResetPassword = () => {
+    navigation.navigate('Login');
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Esqueci minha senha</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite seu e-mail"
+        placeholderTextColor="#FFFFFF"
+        keyboardType="email-address"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleResetPassword}>
+        <Text style={styles.buttonText}>Enviar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.linkButton} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.linkButtonText}>Voltar ao início</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: '#121212',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    color: '#FF9900',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#FF9900',
+    borderRadius: 8,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+    fontSize: 16,
+    color: '#FFFFFF',
+    backgroundColor: '#222222',
+  },
+  button: {
+    backgroundColor: '#FF9900',
+    borderRadius: 8,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    marginTop: 8,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  linkButton: {
+    marginTop: 16,
+  },
+  linkButtonText: {
+    color: '#FF9900',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+  },
+});
+
+export default ForgotPasswordScreen;
